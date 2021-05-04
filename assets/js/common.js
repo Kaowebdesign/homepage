86520bee37766c7294bc5ab8bd9b9159bb4055d9
Andrew <assdwqe@gmail.com>
2021-05-04 15:47:58 +0300

    add js and some effects | upd media query

---
+++ assets/js/common.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    // Parallax elements
+    var elem1= $('.main__parallax_name'),
+        elem2= $('.main__parallax_last-name'),
+        // top position
+        elem2TopPos = elem2.offset().top,
+        // main site container
+        main = $('.main');
+    // Layers, use it for parallax speed
+    var layer2s = 5,
+        layer3s = 7;
+    // Watch user scroll
+    $(window).scroll(function(){
+        // Animate first element
+        elem1.css({'top': -$(window).scrollTop()/layer3s});
+        // Animate second element
+        elem2.css({'top': elem2TopPos - $(window).scrollTop()/layer2s});
+    });
+    // show page preloader
+    main.addClass('loading');
+    // hide page preloader
+    setTimeout(function() {
+        main.removeClass('loading');
+    }, 1500);
+});
